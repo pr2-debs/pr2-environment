5934f50d44d1f5b2e69aaef5947dec530060d7ec
TheDash <noobaca2@gmail.com>
2016-05-10 15:19:54 -0400

    Corrected number of cores

--- root/usr/share/pr2-systemcheck/all/checkcpucores.sh
+++ root/usr/share/pr2-systemcheck/all/checkcpucores.sh
@@ -9,7 +9,7 @@ RV=0
 # 12 cores is a magic number. If not all cores are detected, then mpstat will not display correct
 # number of lines.
 
-if [ $CORE_COUNT != 12 && $CORE_COUNT != 6]; then
+if [ $CORE_COUNT != 12 && $CORE_COUNT != 10]; then
     echo "Not all CPU cores detected. Must have 12 CPU cores enabled. Run \"mpstat -P ALL\" for details" >&2
     RV=1
 fi
